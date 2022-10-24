e6248b8cafe9f330a79bc10a3bf6cb8dfbd839b2
Kishan Kumar <kishankumarkkg@gmail.com>
2022-10-25 00:45:45 +0530

    wait_for_db implemented

---
+++ app/core/my.py
@@ -0,0 +1,23 @@
+# from typing import OrderedDict
+
+
+# dict = {2:"two",3:"three",4:"four",5:"five",6:"six"}
+
+# # print(type(sorted(dict.items(), key = lambda k : (k[1],k[0]))))
+# dict1= sorted(dict.items(), key = lambda k : (k[1],k[0]))
+# print(dict1)
+# for i in dict1.keys():
+#     print(i)
+# ey=0
+# # print(lambda k : (k[1],k[0]))
+# print(dict)
+# print((lambda k : (k[1],k[0]))((1,2)))
+# # print(yella(33), "reper")
+# # print(y)
+# # print(dict.items())
+# # dict1 = OrderedDict(sorted(dict,reverse=True))
+# # for i in dict1.keys():
+# #     print(i)
+# # print(dict1)
+# # # print(type(OrderedDict(sorted(dict.items(),reverse=True))))
+
